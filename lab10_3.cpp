7154f8adfe809d9e4455732a7281e7afd293e500
NotThanaphat <thanapat.pingkaew@gmail.com>
2025-12-23 17:07:07 +0700

    upload lab10 3

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,37 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int count = 0;
+    float sum = 0.0;
+    float sum_of_square = 0.0;
+    float x;
+
+    ifstream source("score.txt");
+
+    while (source >> x)
+    {
+        sum += x;
+        sum_of_square += x * x;
+        count++;
+    }
+
+    float mean = sum / count;
+    float sd = sqrt((sum_of_square / count) - (mean * mean));
+
+    cout << "Number of data = " << count << "\n";
+
+    cout << setprecision(3);
+    cout << "Mean = " << mean << "\n";
+
+
+    cout << fixed << setprecision(3);
+    cout << "Standard deviation = " << sd << endl;
+
+    return 0;
+}
